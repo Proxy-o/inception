f0777eace7da4d8ec00a4fcbcf452eb06c26c46e
proxy-o <you@example.com>
2023-11-26 05:03:29 -0600

    fix url

--- srcs/requirements/wordpress/conf/wp-config.sh
+++ srcs/requirements/wordpress/conf/wp-config.sh
@@ -7,6 +7,7 @@ wp config create	--allow-root \
 	--dbuser=$DB_USER \
 	--dbpass=$DB_PASS \
 	--dbhost=mariadb:3306 
+
 wp core install --url=otait-ta.42.fr --title=otait-ta-Blog --admin_user=othmane --admin_password=1960 --admin_email=othmane@gmail.com
 wp user create auth_user auth@example.com --role=author --user_pass=auth_pass
 
